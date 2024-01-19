282da27e6182dc5fbdc76c8e5d8d873e37df394d
Maxus94 <125117761+Maxus94@users.noreply.github.com>
2024-01-19 15:15:35 +0000

    Deploying to gh-pages from @ Maxus94/goit-js-hw-10@3f7216c4efff09e816e2d35c13d2c49029295bc8 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";const e=document.querySelector(".form");e.addEventListener("submit",r);function r(o){o.preventDefault(),console.log(e.elements),new Promise((s,t)=>{setTimeout(()=>{isSuccess?s("Success! Value passed to resolve function"):t("Error! Error passed to reject function")},2e3)})}
+//# sourceMappingURL=commonHelpers2.js.map
